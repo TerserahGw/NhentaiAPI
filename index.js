2a5209b98aea03a77fc88fcb08dc83b8a441ba0e
TerserahGw <120995106+TerserahGw@users.noreply.github.com>
2024-07-16 20:02:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,11 @@ const port = process.env.PORT || 80;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const deleteFileAfterOneHour = (pdfPath, fileUrl) => {
+const deleteFileAfterOneHour = (pdfPath) => {
     setTimeout(() => {
         if (fs.existsSync(pdfPath)) {
             fs.unlinkSync(pdfPath);
             console.log(`File deleted: ${pdfPath}`);
-            console.log(`File URL deleted: ${fileUrl}`);
         }
     }, 3600000);
 };
@@ -58,7 +57,6 @@ app.get('/doujin', async (req, res) => {
             const randomIndex = Math.floor(Math.random() * data.length);
             const doujin = data[randomIndex];
             const doujinTitle = doujin.title;
-            console.log(doujinTitle);
             const { images } = await doujin.getContents();
 
             const tempDir = '/tmp';
@@ -73,20 +71,38 @@ app.get('/doujin', async (req, res) => {
             const fileUrl = `http://${hostname}/nsfw/${encodeURIComponent(doujinTitle)}.pdf`;
             res.json({ url: fileUrl });
 
-            deleteFileAfterOneHour(pdfFilename, fileUrl);
-
             success = true;
         } catch (error) {
             console.error('Error searching nhentai or saving file:', error);
+            if (!res.headersSent) {
+                res.status(500).send('Internal Server Error');
+            }
         }
     }
 });
 
-app.use('/nsfw', (req, res, next) => {
-    const filePath = path.join('/tmp', req.path);
-    res.setHeader('Content-Disposition', `attachment; filename="${path.basename(filePath)}"`);
-    next();
-}, express.static('/tmp'));
+app.get('/nsfw/:filename', (req, res) => {
+    const filePath = path.join('/tmp', req.params.filename);
+
+    if (fs.existsSync(filePath)) {
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', `attachment; filename="${path.basename(filePath)}"`);
+        
+        const fileStream = fs.createReadStream(filePath);
+        fileStream.pipe(res);
+
+        fileStream.on('end', () => {
+            deleteFileAfterOneHour(filePath);
+        });
+
+        fileStream.on('error', (err) => {
+            console.error('Error streaming file:', err);
+            res.status(500).send('Internal Server Error');
+        });
+    } else {
+        res.status(404).send('File not found');
+    }
+});
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
